2e29c37f916039a20657cb7d6ab98ad2bf79eef1
Harry Hsu <qspiaryb@gmail.com>
2022-09-19 12:24:27 +0800

    Use original c file

--- addons/ci2c.cc
+++ addons/ci2c.cc
@@ -1,57 +1,92 @@
 #include "napi.h"
-#include "i2c.h"
+#include "TBImage.c"
 
 using namespace Napi;
 
-I2CDevice device;
-
-Number Open(const CallbackInfo &info)
+Boolean NAN_Lens_FindMCU(const CallbackInfo &info)
 {
-	device.addr = 0x15;
-	device.tenbit = 0;
-	device.delay = 2;
-	device.flags = 0;
-	device.page_bytes = 256;
-	device.iaddr_bytes = 1;
-	auto busName = info[0].ToString().Utf8Value().c_str();
-	auto ret = i2c_open(busName);
-	if (ret != -1)
-		device.bus = ret;
-	auto env = info.Env();
-	return Number::New(env, ret);
+	return Boolean::New(info.Env(), Lens_FindMCU());
 }
 
-void Close(const CallbackInfo &info)
+Boolean NAN_Lens_ICRMode(const CallbackInfo &info)
+{
+	auto mode = info[0].ToBoolean().Value();
+	return Boolean::New(info.Env(), Lens_ICRMode(mode));
+}
+
+Boolean NAN_Lens_ReadLensData(const CallbackInfo &info)
+{
+	return Boolean::New(info.Env(), Lens_ReadLensData());
+}
+
+Boolean NAN_Lens_WriteLensData(const CallbackInfo &info)
+{
+	return Boolean::New(info.Env(), Lens_WriteLensData());
+}
+
+Number NAN_Lens_GetFocusStep(const CallbackInfo &info)
+{
+	return Number::New(info.Env(), Lens_GetFocusStep());
+}
+
+Number NAN_Lens_Focusmove(const CallbackInfo &info)
+{
+	auto zoomseg = info[0].ToNumber().Uint32Value();
+	return Number::New(info.Env(), Lens_Focusmove(zoomseg));
+}
+
+Number NAN_Lens_GetZoommoveStep(const CallbackInfo &info)
+{
+	return Number::New(info.Env(), Lens_GetZoommoveStep());
+}
+
+Number NAN_Lens_Zoommove(const CallbackInfo &info)
+{
+	auto focusstb = info[0].ToNumber().Uint32Value();
+	return Number::New(info.Env(), Lens_Zoommove(focusstb));
+}
+
+Boolean NAN_Lens_Initial(const CallbackInfo &info)
+{
+	return Boolean::New(info.Env(), Lens_Initial());
+}
+
+Number NAN_Lens_GetPIrisStep(const CallbackInfo &info)
+{
+	return Number::New(info.Env(), Lens_GetPIrisStep());
+}
+
+Boolean NAN_Piris_move(const CallbackInfo &info)
+{
+	auto movesteps = info[0].ToNumber().Uint32Value();
+	return Boolean::New(info.Env(), Piris_move(movesteps));
+}
+
+Boolean NAN_Piris_Initial(const CallbackInfo &info)
+{
+	return Boolean::New(info.Env(), Piris_Initial());
+}
+
+void NAN_i2c_close(const CallbackInfo &info)
 {
 	i2c_close(device.bus);
 }
 
-Number Read(const CallbackInfo &info)
-{
-	auto iaddr = info[0].ToNumber().Uint32Value();
-	auto buf = info[1].As<Buffer<unsigned char>>();
-	auto len = info[2].As<Number>().Uint32Value();
-	auto ret = i2c_ioctl_read(&device, iaddr, buf.Data(), len);
-	auto env = info.Env();
-	return Number::New(env, ret);
-}
-
-Number Write(const CallbackInfo &info)
-{
-	auto iaddr = info[0].ToNumber().Uint32Value();
-	auto buf = info[1].As<Buffer<unsigned char>>();
-	auto len = info[2].As<Number>().Uint32Value();
-	auto ret = i2c_ioctl_write(&device, iaddr, buf.Data(), len);
-	auto env = info.Env();
-	return Number::New(env, ret);
-}
-
 Object Init(Env env, Object exports)
 {
-	exports.Set(String::New(env, "open"), Function::New(env, Open));
-	exports.Set(String::New(env, "close"), Function::New(env, Close));
-	exports.Set(String::New(env, "read"), Function::New(env, Read));
-	exports.Set(String::New(env, "write"), Function::New(env, Write));
+	exports.Set(String::New(env, "i2c_close"), Function::New(env, NAN_i2c_close));
+	exports.Set(String::New(env, "Lens_FindMCU"), Function::New(env, NAN_Lens_FindMCU));
+	exports.Set(String::New(env, "Lens_ICRMode"), Function::New(env, NAN_Lens_ICRMode));
+	exports.Set(String::New(env, "Lens_ReadLensData"), Function::New(env, NAN_Lens_ReadLensData));
+	exports.Set(String::New(env, "Lens_WriteLensData"), Function::New(env, NAN_Lens_WriteLensData));
+	exports.Set(String::New(env, "Lens_GetFocusStep"), Function::New(env, NAN_Lens_GetFocusStep));
+	exports.Set(String::New(env, "Lens_Focusmove"), Function::New(env, NAN_Lens_Focusmove));
+	exports.Set(String::New(env, "Lens_GetZoommoveStep"), Function::New(env, NAN_Lens_GetZoommoveStep));
+	exports.Set(String::New(env, "Lens_Zoommove"), Function::New(env, NAN_Lens_Zoommove));
+	exports.Set(String::New(env, "Lens_Initial"), Function::New(env, NAN_Lens_Initial));
+	exports.Set(String::New(env, "Lens_GetPIrisStep"), Function::New(env, NAN_Lens_GetPIrisStep));
+	exports.Set(String::New(env, "Piris_move"), Function::New(env, NAN_Piris_move));
+	exports.Set(String::New(env, "Piris_Initial"), Function::New(env, NAN_Piris_Initial));
 	return exports;
 }
 
--- src/test.js
+++ src/test.js
@@ -1,12 +1,7 @@
 const ci2c = require("./ci2c");
 
-const ret = ci2c.open("/dev/i2c-2");
-console.log(ret);
-const buf = new Buffer(4);
-const a = ci2c.read(0, buf, 4);
-console.log(a);
-console.log(
-  `Open /dev/i2c-2 MCU 0x15 ID=${buf[0] * 256 + buf[1]} ${
-    buf[2] * 256 + buf[3]
-  } success`
-);
+ci2c.Lens_FindMCU();
+ci2c.Lens_ICRMode(true);
+ci2c.Lens_ReadLensData();
+ci2c.Lens_Initial();
+ci2c.Piris_Initial();
